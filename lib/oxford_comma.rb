84f6da0da35d72f15d68dbb9ce49deb582d80ae8
Rosa Tu <rosalina.tu@gmail.com>
2018-07-07 22:00:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,15 @@
 def oxford_comma(array)
-   if array == 1
-   return array.split
-   elsif array == 2
-     ...array.split("and")
-  elsif 
-     
+   if array.size == 1
+   return array.join
+ end
+   elsif array.size == 2
+  return ...array.join(" and ")
+end
+  elsif array.size == 3
+  array.splice()
+end  
+else
+  return array.split(",")
   end
 end
 
