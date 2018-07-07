8e3eae0edea89ec8156c1fc805021a7f165d8cb5
Rosa Tu <rosalina.tu@gmail.com>
2018-07-07 22:08:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,5 @@
 def oxford_comma(array)
-   if array.size == 1
-   return array.join
- end
-   elsif array.size == 2
-  return ...array.join(" and ")
+   ...array.join(" and ")
 end
   elsif array.size == 3
   lastEl = "and" + array[2]
