00c631af4e1355d645241036a97b448839036265
Rosa Tu <rosalina.tu@gmail.com>
2018-07-07 22:09:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,4 @@
 def oxford_comma(array)
    array.join(" and ")
 end
-  elsif array.size == 3
-  lastEl = "and" + array[2]
-  array.pop(array[2])
-  array << lastEl
-  array.join(",")
-end  
-else
-  return array.join(",")
-  end
-end
 
